33313ccfa89e3797ed725c1c386d0e8ba0afd2c7
Jason Smith (iMac) <jason.h.smith@gmail.com>
2016-07-27 21:54:39 +0700

    The PouchDB-only method

---
+++ 02-pouchdb.js
@@ -0,0 +1,115 @@
+module.exports = make_server
+
+// Demo web application
+// A very rough approximation of a content management system
+//
+
+var Hapi = require('hapi')
+var PouchDB = require('pouchdb')
+var leveldown = require('leveldown')
+
+
+function main() {
+  console.log('Start')
+  make_server(function(er, server) {
+    console.log('ok')
+    if (er)
+      throw er
+
+    server.start(function() {
+      console.log('Server ready', server.info.uri)
+    })
+  })
+}
+
+function make_server(callback) {
+  var server = new Hapi.Server
+  server.connection({host:'0.0.0.0', port:8080})
+
+  // Render a blog post
+  server.route({method:'GET', path:'/{slug}', handler:blog_post})
+
+  // Handlers to let users work with their settings
+  server.route({method:'GET' , path:'/user/{user}/preferences', handler:preferences})
+  server.route({method:'POST', path:'/user/{user}/preferences', handler:preferences})
+
+  //
+  // Server done. Initialize the DBs
+  //
+
+  var blogs = new PouchDB('blogs', {db:leveldown, prefix:__dirname+'/'})
+  var prefs = new PouchDB('prefs', {db:leveldown, prefix:__dirname+'/'})
+
+  server.app.blogs = blogs
+  server.app.prefs = prefs
+
+  // Add the design document to look up blogs by slug.
+  blogs.get('_design/blogs', function(er, doc) {
+    if (er && er.status == 404)
+      doc = {}            // This is fine
+    else if (er)
+      return callback(er) // This is not fine
+
+    doc._id = '_design/blogs'
+    doc.views = {
+      blog_slug: {
+        map: function(doc) {
+          if (doc.slug)
+            emit(doc.slug, doc)
+        }.toString()
+      }
+    }
+
+    blogs.put(doc, function(er) {
+      if (er)
+        return callback(er)
+
+      console.log('Blog ddoc ready')
+      callback(null, server)
+    })
+  })
+}
+
+
+function blog_post(req, reply) {
+  var slug = req.params.slug
+
+  console.log('Look up post: %s', slug)
+  req.server.app.blogs.query('blogs/blog_slug', {key:slug}, function(er, result) {
+    if (er)
+      return reply(er.message).code(500)
+
+    if (result.rows.length == 0)
+      return reply(`<h1>404: ${slug}</h1>`)
+        .code(404)
+
+    // Great! Found the blog post.
+    var doc = result.rows[0].value
+    reply(`<h1>${doc.title}</h1>
+          <p>At ${doc.timestamp}</p>
+          <p>${doc.body}</p>`)
+  })
+}
+
+function preferences(req, reply) {
+  console.log('prefs', req.method)
+  var user = req.params.user
+
+  if (req.method == 'get') {
+    // TODO: Actually do this.
+    var preferences = {}
+
+    console.log('Got preferences for %s: %j', user, preferences)
+    reply(preferences)
+  }
+  
+  else if (req.method == 'post') {
+    // TODO: Actually do this.
+    
+    reply({error:'Not implemented'})
+      .code(500)
+  }
+}
+
+if (require.main === module)
+  main()
